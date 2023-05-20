6a98114d6a508253ef3bd7996113c17e1eb0b45d
TonerowEEtim <anthony.eetim@gmail.com>
2023-05-20 09:59:04 -0700

    0x04. AirBnB clone - Web framework

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
-"""
-from models import storage
 from flask import Flask
+from models import storage, State
 from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown(exceptions):
     storage.close()
 
 
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    state_obj = storage.all("State")
+    states = list()
+    for state, value in state_obj.items():
+        states.append(value)
+    return render_template("7-states_list.html", states=states)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,42 +1,60 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states: HTML page with a list of all State objects.
-    /states/<id>: HTML page displaying the given state with <id>.
-"""
-from models import storage
 from flask import Flask
+from models import storage, State
 from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-def states():
-    """Displays an HTML page with a list of all States.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("9-states.html", state=states)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown(exceptions):
     storage.close()
 
 
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    state_obj = storage.all("State")
+    states = list()
+    for state, value in state_obj.items():
+        states.append(value)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    state_obj = storage.all("State")
+    city_obj = storage.all("City")
+    states = list()
+    cities = list()
+    for state, value in state_obj.items():
+        states.append(value)
+    for city, value in city_obj.items():
+        cities.append(value)
+    return render_template("8-cities_by_states.html",
+                           states=states,
+                           cities=cities)
+
+
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def show_states(id=None):
+    state_obj = storage.all("State")
+    city_obj = storage.all("City")
+    states = list()
+    cities = list()
+    for state, value in state_obj.items():
+        states.append(value)
+    for city, value in city_obj.items():
+        cities.append(value)
+
+    state_id = "State.{}".format(id)
+    if id is not None and state_id not in state_obj:
+        states = None
+    return render_template("9-states.html",
+                           states=states,
+                           cities=cities,
+                           id=id)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
